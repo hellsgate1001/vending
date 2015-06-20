b3fd4ce44040a272ec236074eafc7751231efc35
= <=>
2015-06-20 14:18:17 +0100

    Add sales table info with totals and highlighted popular product

--- assets/js/vending.js
+++ assets/js/vending.js
@@ -1,42 +1,94 @@
 var vending = angular.module('vending', []);
 
 vending.controller('VendingList', function ($scope, $http) {
-    var stock, prices, sales, swp;
+    var stock, prices, sales, swp, totalItems, totalSold, totalSales, maxOrders, popularProduct, popularProductPrice;
+
+    var editPrices = function() {
+        console.log('Edit the prices!!!');
+    }
 
     var getStock = function(){
-        var url = '/assets/json/stock.json'
+        var url = '/assets/json/stock.json';
         return callServer(url, updateStock);
     }
 
     var getPrices = function(){
-        var url = '/assets/json/prices.json'
+        var url = '/assets/json/prices.json';
         return callServer(url, updatePrices);
+    }
+
+    var getSales = function(){
+        var url = '/assets/json/sales.json';
+        return callServer(url, updateSales);
     }
 
     var updateStock = function(data, status){
         stock = data;
         $scope.stock = stock;
         updateStockWithPrices();
-    };
+    }
 
     var updatePrices = function(data, status){
         prices = data;
         $scope.prices = prices;
         updateStockWithPrices();
-    };
+    }
+
+    var updateSales = function(data, status){
+        sales = data;
+        $scope.sales = sales;
+        updateStockWithSales();
+    }
 
     var updateStockWithPrices = function() {
         if (stock && prices) {
+            totalItems  = 0;
             aggregate = {};
             swp = [];
             $(stock).each(function(index, element){
                 aggregate[element.id] = element;
+                totalItems+= element.count;
             });
             $(prices).each(function(index, element){
                 aggregate[element.id].price = element.price;
                 swp.push(aggregate[element.id]);
             });
             $scope.swp = swp;
+            $scope.totalItems = totalItems;
+        }
+    }
+
+    var updateStockWithSales = function() {
+        if (sales) {
+            aggregate = {};
+            totalSold = 0;
+            totalSales = 0;
+            maxOrders = 0;
+            popularProduct = '';
+            popularProductPrice = 0;
+            if (swp) {
+                $(sales).each(function(index, element){
+                    aggregate[element.id] = element;
+                    totalSold+= element.orders.length;
+                    if (element.orders.length > maxOrders) {
+                        maxOrders = element.orders.length;
+                        popularProduct = element.id;
+                        popularProductPrice = element.price;
+                    } else if (element.orders.length == maxOrders && element.price > popularProductPrice) {
+                        popularProduct = element.id;
+                        popularProductPrice = element.price;
+                    }
+                });
+                $(swp).each(function(index, element){
+                    element.orders = aggregate[element.id].orders;
+                    totalSales+= element.orders.length * element.price;
+                });
+            }
+
+            $scope.totalSold = totalSold;
+            $scope.totalSales = totalSales;
+            $scope.popularProduct = popularProduct;
+            $scope.popularProductPrice = popularProductPrice;
         }
     }
 
@@ -46,4 +98,5 @@ vending.controller('VendingList', function ($scope, $http) {
 
     getStock();
     getPrices();
+    getSales();
 });
